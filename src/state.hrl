40e138687e25f50833f1c66d0d49e2dafd8f27be
Łukasz Biedrycki <lukasz.biedrycki@gmail.com>
2014-11-25 17:21:19 +0000

    Make #config{} private

--- src/state.hrl
+++ src/state.hrl
@@ -2,4 +2,5 @@
                  num_watchers = 25 :: pos_integer(),
                  watcher_type = ets :: 'named' | 'ets',
                  dispatch_table :: ets:tid() | 'undefined',
+                 dispatch_mechanism :: 'hash' | 'round_robin',
                  worker_table :: ets:tid() | 'undefined'}).
