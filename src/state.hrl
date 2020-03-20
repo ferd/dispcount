f25f2156c175900c11c046e523cc02cdc40885e9
Andrew Thompson <andrew@hijacked.us>
2020-03-20 16:49:40 -0700

    Switch to callbacks

--- src/state.hrl
+++ src/state.hrl
@@ -2,5 +2,5 @@
                  num_watchers = 25 :: pos_integer(),
                  watcher_type = ets :: 'named' | 'ets',
                  dispatch_table :: ets:tid() | 'undefined',
-                 dispatch_mechanism :: 'hash' | 'round_robin',
+                 dispatch_mechanism = hash :: 'hash' | 'round_robin',
                  worker_table :: ets:tid() | 'undefined'}).
